05383143e64b3498c9828ee966fc9b3dd9035fe0
Ming-Kit Choy <samuelmkchoy@hotmail.com>
2023-12-04 14:57:52 +0000

    here

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,6 +1,5 @@
 /**
  * Created by meudecc on 15/02/2017.
- * fixed by Ming on 4/12/2023.
  */
 public class Student {
 
@@ -8,10 +7,10 @@ public class Student {
 
     public AttendanceGrade getAttendanceGrade(int attendance) {
         if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
-        if (attendance == 0) return AttendanceGrade.ABSENT;
         if (attendance < 30) return AttendanceGrade.VERY_POOR;
+        if (attendance < 50) return AttendanceGrade.POOR;
         if (attendance < 70) return AttendanceGrade.AVERAGE;
-        if (attendance < 90) return AttendanceGrade.GOOD;
+        if (attendance <= 90) return AttendanceGrade.GOOD;
         return AttendanceGrade.VERY_GOOD;
     }
 }
